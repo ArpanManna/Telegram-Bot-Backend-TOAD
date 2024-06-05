11f5056e913a4e14d8b54ca028bf398cef6381ca
ArpanManna <arpan.manna113@gmail.com>
2024-06-05 21:28:28 +0530

    show balance on start implemented

--- bot/bot.js
+++ bot/bot.js
@@ -1,13 +1,14 @@
 import TelegramBot from "node-telegram-bot-api";
-import { botConfig } from "../config/bot.js";
 import mongoose from "mongoose";
 import { User } from "../models/user.js";
 import dotenv from 'dotenv';
+import { config } from "../config.js";
 dotenv.config();
 
 const TELEGRAM_API = `https://api.telegram.org/bot${process.env.BOT_TOKEN}`
 const DB_CONNECTION_URL = "mongodb://localhost:27017/"
 
+// establish DB connection
 mongoose.connect(DB_CONNECTION_URL)
     .then(
         () => console.log("Database connected!")
@@ -16,11 +17,12 @@ mongoose.connect(DB_CONNECTION_URL)
         error => console.log(error)
     )
 
-
+// start Telegram bot instance
 const bot = new TelegramBot(process.env.BOT_TOKEN, {
     polling: true
 })
 
+// update bot webhook events
 async () => {
     await bot.setWebHook("", {
         allowed_updates: JSON.stringify(
@@ -38,23 +40,43 @@ async () => {
 
 
 bot.onText(/\/start/, async (msg, match) => {
-    // console.log(msg)
-    // console.log(match)
     if (match.input == "/start") {
-        const welcome_msg = `Hi, ${msg.chat.first_name}! This is $TOAD, the official memecoin of Toad community
-        Join @toad_test to kick-start your journey with us!`
-        bot.sendMessage(msg.chat.id, welcome_msg, {
-            reply_markup: {
-                inline_keyboard: [
-                    [
-                        {
-                            text: "Join Telegram",
-                            url: "https://t.me/+QVXnumzIcEE0YjQ1",
-                        },
+        const res = await User.find({ chatId: msg.chat.id })
+        if (res.length) {
+            const referral_url=`https://t.me/pikapi_community_bot?start=${msg.chat.id}`
+            bot.sendMessage(msg.chat.id,
+                `Your balance: ${res[0].balance} $TOAD\nReferral count : ${res[0].referralCount}\nYour referral link : ${referral_url}`,
+                {
+                    reply_markup: {
+                        inline_keyboard: [
+                            [
+                                {
+                                    text: "Share 🤝 & Keep earning",
+                                    url: `https://t.me/share/url?url=${referral_url}`,
+                                },
+                            ],
+                        ],
+                    }
+                }
+            )
+        }
+        else {
+            const welcome_msg = `Hi, ${msg.chat.first_name}! This is $${config.symbol}, the official memecoin of Toad community 👋
+        $${config.symbol} is a of friend of $BRETT on Base and $PEPE on EThereum. That's all you ever wanted.
+        Join ${config.groupId} to kick-start your journey with us!`
+            bot.sendMessage(msg.chat.id, welcome_msg, {
+                reply_markup: {
+                    inline_keyboard: [
+                        [
+                            {
+                                text: "Join Telegram",
+                                url: config.telegramJoiningLink,
+                            },
+                        ],
                     ],
-                ],
-            }
-        })
+                }
+            })
+        }
     } else {
         const referredById = match.input.split(" ")[1]
         console.log(match.input.split(" ")[1])
@@ -86,7 +108,7 @@ bot.on('message', async (msg) => {
                     }
                 })
                 // update referrer balance and referral count
-                await User.updateOne({chatId: referredById}, {
+                await User.updateOne({ chatId: referredById }, {
                     $inc: {
                         referralCount: 1,
                         balance: 1000
@@ -110,9 +132,23 @@ bot.on('message', async (msg) => {
         const res = await User.find({ chatId: msg.new_chat_member.id })
         const bal = res[0].balance
         // send message to user chat
+        const referral_url=`https://t.me/pikapi_community_bot?start=${msg.new_chat_member.id}`
         bot.sendMessage(msg.new_chat_member.id,
             `Your balance: ${bal} $TOAD
-            Your referral link : https://t.me/pikapi_community_bot?start=msg.new_chat_member.id`
+            Referral count : 0
+            Your referral link : ${referral_url}`,
+            {
+                reply_markup: {
+                    inline_keyboard: [
+                        [
+                            {
+                                text: "Share 🤝",
+                                url: `https://t.me/share/url?url=${referral_url}`,
+                            },
+                        ],
+                    ],
+                }
+            }
         )
     }
 })
---
+++ config.js
@@ -0,0 +1,6 @@
+export const config = {
+    "botId": "",
+    "groupId": "@toad_test",
+    "symbol": "TOAD",
+    "telegramJoiningLink": "https://t.me/+QVXnumzIcEE0YjQ1"
+}
