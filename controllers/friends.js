06353ff3e8d31254ce68a584dc370fd058d06eb5
ArpanManna <arpan.manna113@gmail.com>
2024-08-15 09:54:33 +0530

    friends endpoint implemented

--- controllers/friends.js
+++ controllers/friends.js
@@ -1,5 +1,21 @@
+import { User } from "../models/user.js"
+
 const getFriends = async(req,res) => {
-    
+    const { chatId } = req.query
+    try{
+        const friends = await User.find({referredBy: chatId})
+        res.status(200).json({
+            success: true,
+            friendLists: friends.slice(0,10),
+            totalFriends: friends? friends.length : 0
+        })
+    }catch(error){
+        res.status(400).json({
+            success: false,
+            error: error
+        })
+    }
+
 }
 
 export default getFriends
